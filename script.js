4585948fb539b705456dc6ddd501c6fff5ae77ad
KARTIK14085 <kartik123489@gmail.com>
2026-08-10 21:37:23 +0530

    simple script with arithmetic operation

---
+++ script.js
@@ -0,0 +1,23 @@
+
+        document.write("Hello World<br>");
+        document.write("Welcome to javascript<br>");
+        console.log("Learning JavaScript");
+        document.write("Name : kartik gurjar<br>");
+        document.write("\nCollege Name : Indus University<br>");
+        document.write("Current Date: " + new Date().toDateString() + "<br>");
+        let a=2;
+        let b=2;
+        document.write("addition= " + (a+b)+"<br>");
+        let l1=23;
+        let b1=2;
+        document.write("area of rectnge =" + (l1*b1)+"<br>");
+        let p=23;
+        let r=2;
+        let t=1;
+        document.write("Interest =" + (p*r*t/100)+"<br>");
+        let y=2;
+        let x=2;
+        let c=2;
+        document.write("Average =" + (y+x+c)/3.0+"<br>");
+  
+
